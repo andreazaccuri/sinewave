82417bc3133a6f8e4dea4957330e806addffdb18
andreazaccuri <andrea.zaccuri@gmail.com>
2018-04-16 18:58:09 +0200

    Responsive sketch + styling

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let cnv;
+
 function setup() {
   // put setup code here
+  cnv = createCanvas(innerWidth, innerHeight);
+  cnv.parent('p5Sketch');
 }
 
 function draw() {
   // put drawing code here
+  background(51);
+}
+
+function windowResize() {
+  resizeCanvas(innerWidth, innerHeight);
 }
